aa1bb252189f25481238197ec8d93f294190db81
MorganGallant <morgan@morgangallant.com>
2019-01-27 13:04:04 -0800

    Base Project for Crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
 /// Efficient Rust Library for Twitter API
 
-#![deny(missing_docs,
-        missing_debug_implementations, missing_copy_implementations,
-        trivial_casts, trivial_numeric_casts,
-        unsafe_code,
-        unstable_features,
-        unused_import_braces, unused_qualifications)]
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub struct Twitter {}
