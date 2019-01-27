7e10d55d3e769eab1ab285b22401b63d8d677f79
MorganGallant <morgan@morgangallant.com>
2019-01-27 12:53:43 -0800

    Crate setup

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
