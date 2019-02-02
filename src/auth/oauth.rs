d44116a3580911e9073906cc399b48e567ca0054
MorganGallant <morgan@morgangallant.com>
2019-02-02 13:00:04 -0800

    Began finalizing oauth headers

--- src/auth/oauth.rs
+++ src/auth/oauth.rs
@@ -1,12 +1,15 @@
+use rand::Rng;
 use std::fmt;
 use url::Url;
 
 use crate::auth::token::Token;
 
+/// This implementation was adopted from https://github.com/azyobuzin/rust-oauthcli.
+
 /// OAuthHeader stores the OAuth1A header for authentication purposes with the Twitter API.
 pub struct OAuthHeader {
     /// The string contents of the header.
-    pub contents: String
+    pub contents: String,
 }
 
 impl OAuthHeader {
@@ -30,39 +33,50 @@ impl fmt::Display for OAuthHeader {
 /// OAuthHeaderBuilder enables the user to construct an OAuthHeader for the OAuth1A Protocol.
 pub struct OAuthHeaderBuilder<'a> {
     /// The HTTP method to use
-    method: &'a str,
-    /// The parameters of the HTTP request 
-    params: Vec<(&'a str, &'a str)>,
+    method: String,
+    /// The parameters of the HTTP request
+    params: Vec<(String, String)>,
     /// The user authentication token
-    token: Token
+    token: &'a Token,
 }
 
 impl<'a> OAuthHeaderBuilder<'a> {
     /// Create a new OAuthHeaderBuilder instance.
-    pub fn new(method: &'a str, token: Token) -> Self {
+    pub fn new(method: String, token: &'a Token) -> Self {
         OAuthHeaderBuilder {
             method,
             params: Vec::new(),
-            token
+            token,
         }
     }
 
     /// Add a set of request parameters to the header.
     pub fn parameters<P>(&mut self, parameters: P) -> &mut Self
-    where P: IntoIterator<Item=(&'a str, &'a str)>
+    where
+        P: IntoIterator<Item = (String, String)>,
     {
         self.params.extend(parameters.into_iter());
         self
     }
 
     /// Add a new parameter to the header.
-    pub fn add_param(&mut self, key: &'a str, value: &'a str) {
+    pub fn add_param(&mut self, key: String, value: String) {
         self.params.push((key, value));
     }
 
     /// Finalize the creation of the OAuthHeader.
-    pub fn finalize() -> OAuthHeader {
+    pub fn finalize(&mut self) -> OAuthHeader {
+        self.add_param(
+            "oauth_consumer_key".to_string(),
+            self.token.consumer_key.clone(),
+        );
+        self.add_param("oauth_token".to_string(), self.token.access_token.clone());
+        self.add_param("oauth_signature_method".to_string(), "HmacSha1".to_string());
+        self.add_param(
+            "oauth_timestamp".to_string(),
+            time::now_utc().to_timespec().sec.to_string(),
+        );
+
         unimplemented!();
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 /*!
-This crate contains client bindings for the [Twitter API](https://developer.twitter.com/content/developer-twitter/en.html). It is meant to be an easy bridge from Rust to Twitter, and allow the easy creation of applications involving the Twitter API. 
+This crate contains client bindings for the [Twitter API](https://developer.twitter.com/content/developer-twitter/en.html). It is meant to be an easy bridge from Rust to Twitter, and allow the easy creation of applications involving the Twitter API.
  */
 #![deny(missing_docs)]
 
+extern crate rand;
+extern crate time;
 extern crate url;
 
 /// The authentication module handles all user authentication, including interfacing with the Twitter OAuth1A standard.
 pub mod auth;
-
